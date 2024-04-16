73baba1430ab9ab91582e2317d569e2666dab631
zabelkin <alex.zabelkin@gmail.com>
2024-04-16 21:37:04 +0300

    123

--- hw.cpp
+++ hw.cpp
@@ -8,8 +8,16 @@ using namespace std;
 
 int main()
 {
-    std::string string("hello $name");
-    // string = std::regex_replace(string, std::regex("\\ "), "-");
-    cout << string.replace(string.begin(), string.end(), "$name", "Someone!");
+    int max_arr = 20;
+    int arr_odds[max_arr];
+
+    for(int i=0; i<max_arr; i++) {
+        arr_odds[i]=2*i+1;
+    }
+
+    for(int i=0; i<max_arr; i++) {
+        cout << *(arr_odds+i) << ((i+1)%5==0?"\n":"\t");
+    }
+
     return 0;
 }
